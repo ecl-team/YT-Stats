60ed73e2562807a2d6ae6ceeb8dca9fe94f0234b
Eclipsum <wozzswe@gmail.com>
2018-12-10 18:19:54 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,32 @@
-function getPageContent(url) {
+function element(id) {
+	return document.getElementById(id);
+}
+function getPageContent(url = String()) {
 	var req = new XMLHttpRequest();
 	req.open("GET", url, false);
 	req.send();
 	return req.responseText;
 }
-function Json(url) {
+function Json(url = String()) {
 	return JSON.parse(getPageContent(url));
 }
-
-function channelJson(part, channel) {
+function channelJson(part = String(), channel = String()) {
 	return Json("https://www.googleapis.com/youtube/v3/channels?part=" + part + "&id=" + channel.id + "&key=AIzaSyBZuZRr7b-LpYKHl4IilXp1ZCrSh64dznc");
 }
-function channelJsonFromId(part, id) {
+function channelJsonFromId(part = String(), id = String()) {
 	return Json("https://www.googleapis.com/youtube/v3/channels?part=" + part + "&id=" + id + "&key=AIzaSyBZuZRr7b-LpYKHl4IilXp1ZCrSh64dznc");
 }
-function channelJsonFromName(part, name) {
+function channelJsonFromName(part = String(), name = String()) {
 	return Json("https://www.googleapis.com/youtube/v3/channels?part=" + part + "&forUsername=" + name + "&key=AIzaSyBZuZRr7b-LpYKHl4IilXp1ZCrSh64dznc");
 }
+function videoJson(part = String(), video = String()) {
+	return Json("https://www.googleapis.com/youtube/v3/videos?part=" + part + "&id=" + video.id + "&key=AIzaSyBZuZRr7b-LpYKHl4IilXp1ZCrSh64dznc");
+}
+function videoJsonFromId(part = String(), id = String()) {
+	return Json("https://www.googleapis.com/youtube/v3/videos?part=" + part + "&id=" + id + "&key=AIzaSyBZuZRr7b-LpYKHl4IilXp1ZCrSh64dznc");
+}
 class Channel {
-	constructor(channel) {
+	constructor(channel = String()) {
 		if (channel.length == 24)
 			this.id = channel;
 		else
@@ -37,30 +45,46 @@ class Channel {
 		return parseInt(channelJson("statistics", this)["items"][0]["statistics"]["videoCount"]);
 	}
 }
-
-function videoJson(part, video) {
-	return Json("https://www.googleapis.com/youtube/v3/videos?part=" + part + "&id=" + video.id + "&key=AIzaSyBZuZRr7b-LpYKHl4IilXp1ZCrSh64dznc");
-}
-function videoJsonFromId(part, id) {
-	return Json("https://www.googleapis.com/youtube/v3/videos?part=" + part + "&id=" + id + "&key=AIzaSyBZuZRr7b-LpYKHl4IilXp1ZCrSh64dznc");
-}
 class Video {
-	constructor(video) {
+	constructor(video = String()) {
 		this.id = video;
 	}
 	name() {
-		return channelJson("snippet", this)["items"][0]["snippet"]["title"];
+		return videoJson("snippet", this)["items"][0]["snippet"]["title"];
 	}
 	views() {
-		return parseInt(channelJson("statistics", this)["items"][0]["statistics"]["viewCount"]);
+		return parseInt(videoJson("statistics", this)["items"][0]["statistics"]["viewCount"]);
 	}
 	likes() {
-		return parseInt(channelJson("statistics", this)["items"][0]["statistics"]["likeCount"]);
+		return parseInt(videoJson("statistics", this)["items"][0]["statistics"]["likeCount"]);
 	}
 	dislikes() {
-		return parseInt(channelJson("statistics", this)["items"][0]["statistics"]["dislikeCount"]);
+		return parseInt(videoJson("statistics", this)["items"][0]["statistics"]["dislikeCount"]);
 	}
 	comments() {
-		return parseInt(channelJson("statistics", this)["items"][0]["statistics"]["commentCount"]);
+		return parseInt(videoJson("statistics", this)["items"][0]["statistics"]["commentCount"]);
+	}
+}
+var liveSources = [];
+function startLive(id, func) {
+	stopLive(id);
+	liveSources.push({
+		id: id,
+		interval: setInterval(func, 1000)
+	});
+}
+function startLive(id, func, interval) {
+	stopLive(id);
+	liveSources.push({
+		id: id,
+		interval: setInterval(func, interval)
+	});
+}
+function stopLive(id) {
+	for (i = 0; i < liveSources.length; i++) {
+		if (liveSources[i].id == id) {
+			clearInterval(liveSources[i].interval);
+			liveSources.splice(i, 1);
+		}
 	}
 }
