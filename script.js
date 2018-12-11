11491e35f1663d370ef5b917045e78e2bbbcf387
Eclipsum <wozzswe@gmail.com>
2018-12-11 12:59:34 +0100

    Update script.js

--- script.js
+++ script.js
@@ -31,15 +31,42 @@ class Channel {
 			this.id = channel;
 		else
 			this.id = channelJsonFromName("snippet", channel)["items"][0]["id"];
-		this.lastData
+		this.lastData = {
+			subs: 0,
+			views: 0,
+			uploads: 0
+		};
 		this.events = [];
 		this.eventListener = setInterval(() => {
-			for (i = 0; i < events.length; i++) {
+			for (var i = 0; i < this.events.length; i++) {
 				switch(this.events[i].event) {
-					case "subscription": break;
+					case "subs": {
+						var subs = parseInt(channelJson("statistics", this)["items"][0]["statistics"]["subscriberCount"]);
+						if (subs != this.lastData.subs) {
+							this.events[i].func(subs - this.lastData.subs);
+							this.lastData.subs = subs;
+						}
+						break;
+					}
+					case "views": {
+						var views = parseInt(channelJson("statistics", this)["items"][0]["statistics"]["viewCount"]);
+						if (views != this.lastData.views) {
+							this.events[i].func(views - this.lastData.views);
+							this.lastData.views = views;
+						}
+						break;
+					}
+					case "uploads": {
+						var uploads = parseInt(channelJson("statistics", this)["items"][0]["statistics"]["videoCount"]);
+						if (uploads != this.lastData.uploads) {
+							this.events[i].func(uploads - this.lastData.uploads);
+							this.lastData.uploads = uploads;
+						}
+					}
+					break;
 				}
 			}
-		}, 500);
+		}, 1000);
 	}
 	name() {
 		return channelJson("snippet", this)["items"][0]["snippet"]["title"];
@@ -63,6 +90,51 @@ class Channel {
 class Video {
 	constructor(video = String()) {
 		this.id = video;
+		this.lastData = {
+			views: 0,
+			likes: 0,
+			dislikes: 0,
+			comments: 0
+		};
+		this.events = [];
+		this.eventListener = setInterval(() => {
+			for (var i = 0; i < this.events.length; i++) {
+				switch(this.events[i].event) {
+					case "views": {
+						var views = parseInt(videoJson("statistics", this)["items"][0]["statistics"]["viewCount"]);
+						if (views != this.lastData.views) {
+							this.events[i].func(views - this.lastData.views);
+							this.lastData.views = views;
+						}
+						break;
+					}
+					case "likes": {
+						var likes = parseInt(videoJson("statistics", this)["items"][0]["statistics"]["likeCount"]);
+						if (likes != this.lastData.likes) {
+							this.events[i].func(likes - this.lastData.likes);
+							this.lastData.likes = likes;
+						}
+						break;
+					}
+					case "dislikes": {
+						var dislikes = parseInt(videoJson("statistics", this)["items"][0]["statistics"]["dislikeCount"]);
+						if (dislikes != this.lastData.dislikes) {
+							this.events[i].func(dislikes - this.lastData.dislikes);
+							this.lastData.dislikes = dislikes;
+						}
+						break;
+					}
+					case "comments": {
+						var comments = parseInt(videoJson("statistics", this)["items"][0]["statistics"]["commentCount"]);
+						if (comments != this.lastData.comments) {
+							this.events[i].func(comments - this.lastData.comments);
+							this.lastData.comments = comments;
+						}
+						break;
+					}
+				}
+			}
+		}, 1000);
 	}
 	name() {
 		return videoJson("snippet", this)["items"][0]["snippet"]["title"];
@@ -89,6 +161,12 @@ class Video {
 	}
 	comments() {
 		return parseInt(videoJson("statistics", this)["items"][0]["statistics"]["commentCount"]);
+	}
+	on(event, func) {
+		this.events.push({
+			event: event,
+			func: func
+		});
 	}
 }
 var liveSources = [];
